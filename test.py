4fba76283bee51c4d094c855dee803e086deb8c6
Tiago Tresoldi <tresoldi@shh.mpg.de>
2020-03-31 10:41:04 +0200

    Add tests

--- test.py
+++ test.py
@@ -5,3 +5,13 @@ from __future__ import unicode_literals
 def test_valid(cldf_dataset, cldf_logger):
     assert cldf_dataset.validate(log=cldf_logger)
 
+def test_forms(cldf_dataset):
+    assert len(list(cldf_dataset["FormTable"])) == 9030
+    assert any(f["Form"] == "jãpirika" for f in cldf_dataset["FormTable"])
+
+def test_parameters(cldf_dataset):
+    assert len(list(cldf_dataset["ParameterTable"])) == 128
+
+
+def test_languages(cldf_dataset):
+    assert len(list(cldf_dataset["LanguageTable"])) == 69
